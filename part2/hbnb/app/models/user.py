760acfd71a83695ba9dea162cc48cfe7087a0907
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-06-16 22:01:28 +0000

    FEATURE : inherit UserClass with Basemodel

---
+++ part2/hbnb/app/models/user.py
@@ -0,0 +1,8 @@
+from basemodel import BaseModel
+
+class User(BaseModel):
+    def __init__(self, first_name, last_name, email, is_admin):
+        super().__init__()
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
