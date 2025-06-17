5e6ea914677efd767997eab0e2e56e68e633d2ca
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-06-17 12:48:34 +0000

    def a function dict to print the data into a library and started the class places

--- part2/hbnb/app/models/user.py
+++ part2/hbnb/app/models/user.py
@@ -7,3 +7,15 @@ class User(BaseModel):
         self.last_name = last_name
         self.email = email
         self.is_admin = is_admin
+
+    
+    def to_dict(self):
+        """Convertit l'objet en dico pour le print"""
+        return {
+            "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "email": self.email,
+            "created_at": self.created_at.isoformat(),
+            "updated_at": self.updated_at.isoformat(),
+        }
