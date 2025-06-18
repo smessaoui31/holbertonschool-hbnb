13cb8cf21d22732dc42b794cfc6886261c25608d
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-06-18 13:58:06 +0000

    FEATURE: Rearrange methods in HBnBFacade for better organization and clarity

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -22,17 +22,20 @@ class HBnBFacade:
         return self.user_repo.get_by_attribute('email', email)
 
     # Placeholder method for fetching a place by ID
-    def get_place(self, place_id):
-        # Logic will be implemented in later tasks
-        pass
-
     def create_place(self, place_data):
+        # Placeholder for logic to create a place, including validation for price, latitude, and longitude
         pass
 
-    def get_all_places(self,):
-        # Logic to retrieve all places
-
-    def update_place(self , place_id, place_data):
-        # logic to update the places
+    def get_place(self, place_id):
+        # Placeholder for logic to retrieve a place by ID, including associated owner and amenities
         pass
+
+    def get_all_places(self):
+        # Placeholder for logic to retrieve all places
+        pass
+
+    def update_place(self, place_id, place_data):
+        # Placeholder for logic to update a place
+        pass
+
 facade = HBnBFacade()
