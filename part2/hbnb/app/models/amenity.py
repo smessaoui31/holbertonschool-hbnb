029e2fe9f002e4f1ff757e4003076348312a22aa
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-06-19 07:49:11 +0000

    FEATURE: Add validation for amenity name in the Amenity model

--- part2/hbnb/app/models/amenity.py
+++ part2/hbnb/app/models/amenity.py
@@ -3,4 +3,18 @@ from .basemodel import BaseModel
 class Amenity(BaseModel):
     def __init__(self, name):
         super().__init__()
-        self.name = name
+
+        if not isinstance(name, str) or not name.strip():
+            raise ValueError ("The amenities name must be a string available")
+        
+        self.name = name.strip()
+
+
+    def __str__(self):
+        # Retourne une représentation claire de l'amenity
+        return f"[Amenity] ({self.id}) {self.name}"
+    
+    def to_dict(self):
+        dict_repr = super().to_dict()  # hérite probablement d'un to_dict dans BaseModel
+        dict_repr["name"] = self.name
+        return dict_repr
