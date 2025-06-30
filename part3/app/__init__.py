c79562a89a2b2039a9f639bad8cf6d93a8e5f5f2
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-06-30 13:55:07 +0000

    REFactor: Update create_app function to accept configuration name and initialize database

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,13 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.amenities import api as amenities_ns
 
+db = SQLAlchemy()
 
-def create_app():
+def create_app(config_name="default"):
+
+    from config import config # Importe le dictionnaire
     app = Flask(__name__)
+    app.config.from_object(config[config_name])
+
+    db.init_app(app)
+    
     api = Api(app, version='1.0', title='HBnB API',
               description='HBnB Application API')
 
