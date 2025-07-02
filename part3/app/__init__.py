594b9724d5ec4b4fc586726874a2c16ca9689f07
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-07-02 15:13:55 +0000

    Add authentication namespace to API

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -7,6 +7,7 @@ from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.amenities import api as amenities_ns
 from flask_jwt_extended import JWTManager
+from app.api.v1.auth import api as auth_ns
 
 db = SQLAlchemy()
 jwt = JWTManager()
@@ -29,5 +30,6 @@ def create_app(config_name="default"):
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    api.add_namespace(auth_ns, path='/api/v1/auth')
     
     return app
