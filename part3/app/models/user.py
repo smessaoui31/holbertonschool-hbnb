a135d6b68d67bf2076e31cba3f332263973da310
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-07-02 15:14:17 +0000

    Remove unnecessary blank line in User model

--- part3/app/models/user.py
+++ part3/app/models/user.py
@@ -28,7 +28,6 @@ class User(BaseModel):
         
     def check_password(self, password):
         return bcrypt.check_password_hash(self.password_hash, password)
-
     
     def to_dict(self):
         """Convertit l'objet en dico pour le print"""
