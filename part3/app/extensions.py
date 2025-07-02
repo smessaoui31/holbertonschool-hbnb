71e405c08efcabfddb6c99d972ccb86270e5aedb
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-07-02 14:35:43 +0000

    Add JWTManager import to extensions.py

--- part3/app/extensions.py
+++ part3/app/extensions.py
@@ -1,6 +1,8 @@
 # app/extensions.py
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
+jwt = JWTManager()
