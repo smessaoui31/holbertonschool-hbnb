de83e5135a17a68004db6354e09664a4fa000e55
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-07-07 13:33:35 +0000

    Refactor BaseModel to inherit from db.Model and define database columns

--- part3/app/models/basemodel.py
+++ part3/app/models/basemodel.py
@@ -1,8 +1,14 @@
 import uuid
 from datetime import datetime
+from app import db
 
 
-class BaseModel:
+class BaseModel(db.Model):
+    __abstract__ = True
+    
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
     def __init__(self):
         self.id = str(uuid.uuid4())
