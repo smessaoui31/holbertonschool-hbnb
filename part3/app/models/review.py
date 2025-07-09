8aedda16c2b2074ae377abe587c6988e5ff411bb
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-07-09 14:13:52 +0000

    Add Review model with text and rating fields

--- part3/app/models/review.py
+++ part3/app/models/review.py
@@ -1,6 +1,12 @@
 from .basemodel import BaseModel
+from app import db
 
 class Review(BaseModel):
+    __tablename__ = "reviews"
+
+    text = db.Column(db.Text, nullable = False)
+    rating = db.Column(db.Integer, nullable = False)
+    
     def __init__(self, text, user_id, place_id, rating):
         super().__init__()
         self.text = text
