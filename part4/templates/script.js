b50a8aa34dd5452b8101b5ad52fd6195d67ab468
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-07-22 14:20:53 +0000

    testing to login and call the js for the api

--- part4/templates/script.js
+++ part4/templates/script.js
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   // Charger les places depuis l’API
-  fetch('http://localhost:5000/api/v1/places/')
+  fetch('http://127.0.0.1:5000/api/v1/places/')
     .then(response => response.json())
     .then(data => {
       placesList.innerHTML = '';
