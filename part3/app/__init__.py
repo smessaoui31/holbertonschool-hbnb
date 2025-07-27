a3d5c2a9ccfcd579c85acad44f799f9dec2441f2
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-07-27 13:36:38 +0000

    Linked the front iwith the back

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -24,7 +24,7 @@ def create_app(config_name="default"):
     bcrypt.init_app(app)
     jwt.init_app(app)
     
-    CORS(app, ressources={r"/api/*":{"origins": "http://127.0.0.1:5500"}})
+    CORS(app, ressources={r"/api/*":{"origins": "http://localhost:5500"}})
     
     api = Api(app, version='1.0', title='HBnB API',
               description='HBnB Application API')
