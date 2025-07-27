2e1c60a6040b057fc0ab8200d475a2bb31792da4
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-07-27 12:35:58 +0000

    FIX wrong line on the os.getenv

--- part3/config.py
+++ part3/config.py
@@ -1,28 +1,25 @@
 import os
 from datetime import timedelta
 
-
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
     DEBUG = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=2)
 
-
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.getenv(
-    'sqlite:///development.db',
-    'sqlite:///app.db' # Chemin vers le fichier Sqlite
+        'SQLALCHEMY_DATABASE_URI', 
+        'sqlite:///app.db'  # Chemin vers le fichier SQLite par défaut
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
 
 class ProductionConfig(Config):
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
 
-
+# Mapping des configurations
 config = {
     'development': DevelopmentConfig,
     'default': DevelopmentConfig,
